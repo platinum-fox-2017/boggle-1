91658aec98efd2884eb407a6f47ad571ea115f8e
agungcaproex <agung.caproex@gmail.com>
2018-02-11 16:03:17 +0700

    release 1 tak usai

--- boggle.js
+++ boggle.js
@@ -1,9 +1,11 @@
 "use strict"
 
 class Boggle{
-  constructor(){
+  constructor(size){
     this.board_game = []
+    this.size = size || 3
     this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    this.dict = dict_string.split(',')
 
   }
 
@@ -12,9 +14,9 @@ class Boggle{
   }
 
   board(size){
-    for(let i=0; i<size; i++){
+    for(let i=0; i<this.size; i++){
       this.board_game.push([])
-      for(let j=0; j<size; j++){
+      for(let j=0; j<this.size; j++){
         let random = this.alphabet[Math.floor(Math.random() * this.alphabet.length)];
         this.board_game[i].push(random)
       }
@@ -22,11 +24,38 @@ class Boggle{
     return this.board_game
   }
 
+  search(){
+    let word = []
+    let firstIndex = []
+    let index = ''
+    let count = 0
+    while(count < this.dict.length){
+      for(let i=0; i<this.board_game.length; i++){
+        // console.log(this.board_game[i])
+        for(let j=0; j<this.board_game[i].length; j++){
+          index = this.dict[count]
+          firstIndex = index[0]
+          if(this.board_game[i][j] === firstIndex && word.indexOf(index) === -1){
+            word.push(this.dict[count])
+          }
+        }
+      }
+      count ++
+    }
+    return word
+  }
+
+  check_pos(){
+
+  }
+
 }
 
 var fs = require('fs')
-var dict_string = fs.readFileSync('dict.txt').toString()
+var dict_string = fs.readFileSync('dict.txt').toString().split("\n")[0]
 
-console.log(dict_string)
-var game = new Boggle()
-console.log(game.board(9))
+
+var game = new Boggle(4)
+console.log(game.board())
+// console.log(game.dict.length)
+console.log(game.search())
