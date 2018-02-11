ebc8de1b4df6ccd3cbcad9932791d707a1fb79b2
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-02-11 11:18:27 +0700

    Find words in boggle

--- boggle.js
+++ boggle.js
@@ -3,6 +3,9 @@ class Boggle {
   constructor(dictionary){
     this.dictionary = dictionary;
     this.board = this.shake();
+    this.size = 4;
+    this.wordFound = [];
+    this.boxVisited = this.setAllToUnvisited();
   }
 
   shake(){
@@ -23,7 +26,106 @@ class Boggle {
      return wholeBoard;
   }
   solve(){
+    let wordFound = [];
+    for(var i = 0; i < this.dictionary.length; i++){
+       this.boxVisited = this.setAllToUnvisited();
+       if(this.isFoundWord(this.dictionary[i])) {
+         wordFound.push(this.dictionary[i]);
+       }
+    }
+    this.wordFound = wordFound;
+
+  }
+  setAllToUnvisited(){
     
+    let arr = [];
+    for(var i = 0; i < this.size; i++){
+      let row = [];
+      for(var j = 0; j < this.size; j++)  {
+        row.push(false);
+      }
+      arr.push(row);
+    }
+    return arr;
+    
+  }
+  isFoundWord(word){
+    let indexFirstChar = this.checkFirstChar(word[0]); 
+    if(indexFirstChar === -1) return false;
+    if(indexFirstChar.length > 1){
+      var isFound = false;
+      for(var i = 0; i < indexFirstChar.length; i++){
+        if(this.checkNextChar(word,indexFirstChar[i].row,indexFirstChar[i].col))  {
+          isFound = true;
+        }
+      }
+      if(!isFound){
+       return false;  
+      }
+    } else {
+      if(!this.checkNextChar(word,indexFirstChar[0].row,indexFirstChar[0].col)) return false;
+    }
+    return true;
+  }
+  checkFirstChar(char){
+    var indexChar = [];
+    for(var i = 0 ; i < this.board.length; i++)  {
+      for(var j = 0; j < this.board[i].length; j++)  {
+        if(char == this.board[i][j])  {
+          indexChar.push({row: i, col: j});
+        }
+      }
+    }
+    if(indexChar.length === 0){
+      return -1 ;
+    } else {
+      return indexChar ;
+    }
+  }
+  checkNextChar(char,row,col){
+    //sampai akhir gak ada yang salah maka word nya benar
+    if(char.length === 0) {
+      return true;  
+    }
+    if(!this.isSameChar(char[0],row,col)) return false;
+    //atas
+    if(this.checkNextChar(char.slice(1),row - 1, col)) return true;
+    //atas kanan
+    if(this.checkNextChar(char.slice(1),row - 1, col + 1)) return true;
+    //atas kiri
+    if(this.checkNextChar(char.slice(1),row - 1, col - 1)) return true;
+    //bawah
+    if(this.checkNextChar(char.slice(1),row + 1, col)) return true;
+    // bawah kanan
+    if(this.checkNextChar(char.slice(1),row + 1, col + 1)) return true;
+    //bawah kiri
+    if(this.checkNextChar(char.slice(1),row + 1, col - 1)) return true;
+    //kanan
+    if(this.checkNextChar(char.slice(1),row, col + 1)) return true;
+    //kiri
+    if(this.checkNextChar(char.slice(1),row, col - 1)) return true;
+    this.boxVisited[row][col] = true;
+    return false;
+  }
+  isSameChar(char,row,col){
+    if(this.isStillInBoard(row,col)){
+      this.boxVisited[row][col] = true;
+      if(this.board[row][col] == char) {
+        return true;   
+      }
+    }
+    return false;
+    
+  }
+  isStillInBoard(row,col){
+    if(row >= this.size || col >= this.size)  {
+     return false;  
+    } else if(row < 0 || col < 0 ){
+     return false; 
+    } else if( this.boxVisited[row][col]){
+     return false;  
+    }
+    return true;
   }
   randomChar(){
     var possible = "abcdefghijklmnopqrstuvwxyz";
@@ -35,9 +137,17 @@ class Boggle {
     }
     console.log(this.board.join('\n'));
   }
+  printWordFound(){
+    if(this.wordFound.length === 0)  {
+      console.log("No Word Found!");
+    } else {
+      console.log('Words Found :',this.wordFound.join(', '))  
+    }
+  }
 }
 
 const boggle = new Boggle(dictionary);
 
-console.log(boggle.dictionary);
 boggle.printBoard();
+boggle.solve();
+boggle.printWordFound();
