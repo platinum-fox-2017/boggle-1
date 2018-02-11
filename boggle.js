e5c768741574e32482f25e34f1210e9d10676d41
srohimah <sitirohimach@gmail.com>
2018-02-11 15:14:41 +0700

    fixed double result

--- boggle.js
+++ boggle.js
@@ -1,49 +1,43 @@
 class Boogle{
-    constructor (boardLength){
-        this.boardLength = boardLength      
-        this.board = this.boogleBoard()
-        this.dict = ['DAY','JOIN','HEART','YEAR']
-        this.firstCoordinate = this.checkFirstLetter()
+    constructor (){     
+        this.board = this.shake(4)
+        this.dict = ['DAY','JOIN','HEART','YEAR','MONTH']
     }
 
-    boogleBoard(){
+    shake(length){
         let sample = 'DMONAYHTEHRAARTJ'
         let counter = 0
         let board = []
-        for (let i=0; i<this.boardLength; i++){
+        for (let i=0; i<length; i++){
             board.push([])
-            for(let j=0; j<this.boardLength; j++){
+            for(let j=0; j<length; j++){
                 board[i].push(sample[counter])
                 counter++
             }
         }
         return board
     }
-    
-    checkFirstLetter(){
+  
+    checkFirstLetter(word){
         let firstChars = []
         for(let i=0; i<this.board.length; i++){         
             for(let j=0; j<this.board.length; j++){
-                for(let k=0; k<this.dict.length; k++){
-                    if(this.board[i][j] === this.dict[k].charAt(0)){
-                        firstChars.push([i,j])
-                    }
-                }
+                if(this.board[i][j] === word.charAt(0)) firstChars.push([i,j])                   
             }
         }
        return firstChars;
     }
 
     checkVisibleMove(word, slicedWord, start, visited, result){
-        if(slicedWord.length === 0){
-            result.push(word);
-            return result
-        }
         let path = [[-1, -1],[-1, 0],[-1, 1],[0, -1],[0, 1],[1, -1],[1, 0],[1, 1]]
-        for(let i = 0; i<path.length-1; i++){
+        for(let i = 0; i<path.length; i++){
         let nextRow = start[0] + path[i][0]
         let nextCol = start[1] + path[i][1]
-            if(0<=nextRow && nextRow<this.board.length && 0<=nextCol && nextCol<this.board.length){
+
+        // console.log('visited : ',visited)
+        // console.log('next :',[nextRow, nextCol])
+        
+            if(nextRow>=0 && nextRow<this.board.length && nextCol>=0 && nextCol<this.board.length){
                 if(this.move([nextRow, nextCol], visited)){
                     if(this.board[nextRow][nextCol] === slicedWord[0] ){
                         let newStart = [nextRow, nextCol]
@@ -61,25 +55,7 @@ class Boogle{
         }
      }
 
-    solve(){
-        let result = []
-        let start = this.firstCoordinate
-        for(let i = 0; i<this.dict.length; i++){
-            let word = this.dict[i]
-            if(start.length>0){
-                let newWord = word.slice(1)
-                for(let j = 0; j<start.length; j++){
-                    this.checkVisibleMove(word, newWord, start[i], [start[i]],result)
-                }
-                
-            }
-        }
-        console.log("Found words are:");
-        for (let i = 0; i < result.length; i++) {
-            console.log(result[i]);
-        }
-    }
-
+    
     move(tested, visited){
         for(let i = 0; i<visited.length; i++){
             if(tested[0] === visited[i][0] && tested[1] === visited[i][1]){
@@ -89,10 +65,28 @@ class Boogle{
         return true
     }
 
+    solve(){
+        let result = []
+          for(let i = 0; i<this.dict.length; i++){
+          let word = this.dict[i]
+          let start = this.checkFirstLetter(word)
+          if(start.length>0){
+              let newWord = word.slice(1)
+              for(let i = 0; i<start.length; i++){
+                  this.checkVisibleMove(word, newWord, start[i], [start[i]],result)
+              }
+              
+          }
+      }
+      console.log(`${result.length} words found :`);
+      for (let i = 0; i < result.length; i++) {
+          console.log(result[i]);
+      }
+  }
 
 
 }
 
-let boogle = new Boogle(4)
-console.log(boogle.boogleBoard())
-console.log(boogle.solve())
+let boogle = new Boogle()
+console.log(boogle.shake(4))
+boogle.solve()
